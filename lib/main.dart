ba55f768866c1f25aed3452d3045c644fe078afe
Haniel Lohn <hanielohn2016@gmail.com>
2022-08-31 21:44:55 -0300

    add: questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_quizz/question.dart';
 
 void main() {
   runApp(const SuperQuiz());
@@ -13,6 +14,36 @@ class SuperQuiz extends StatefulWidget {
 
 class _SuperQuizState extends State<SuperQuiz> {
   List<Icon> score = [];
+  List<Question> questionBank = [
+    Question('Alguns gatos são alérgicos a humanos', true),
+    Question(
+        'É possível fazer com que uma vaca suba escadas, mas não descê-las.',
+        false),
+    Question(
+        'Aproximadamente um quarto dos ossos humanos estão nos pés.', true),
+    Question('O sangue de uma lesma é verde.', true),
+    Question('O nome de solteira da mãe de Buzz Aldrin era \"Moon\".', true),
+    Question('É ilegal urinar no oceano, em Portugal.', true),
+    Question(
+        'Nenhum pedaço quadrado de papel pode ser dobrado mais do que 7 vezes.',
+        false),
+    Question(
+        'Em Londres, se você morrer no Parlamento, você tem direito a um funeral de estado, porque o prédio é considerado um local sagrado.',
+        true),
+    Question(
+        'O som mais forte produzido por qualquer animal é de 188dB. Esse animal é o elefante africano.',
+        false),
+    Question(
+        'A área total dos dois pulmões de um humano é de aproximadamente 70 metros quadrados.',
+        true),
+    Question('Google foi chamado, originalmente de \"Backrub\".', true),
+    Question(
+        'O chocolate afeta o coração e o sistema nervoso de um cão; algumas gramas são suficientes para matar um cão pequeno.',
+        true),
+    Question(
+        'Na Virginia do Oeste, EUA, se você atropelar um animal com um carro, por acidente, você pode levá-lo para casa e comê-lo.',
+        true),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -31,15 +62,15 @@ class _SuperQuizState extends State<SuperQuiz> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              const Expanded(
+              Expanded(
                 flex: 5,
                 child: Padding(
-                  padding: EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(10.0),
                   child: Center(
                     child: Text(
-                      "Aqui vem a pergunta.",
+                      questionBank[0].text,
                       textAlign: TextAlign.center,
-                      style: TextStyle(
+                      style: const TextStyle(
                         fontSize: 25.0,
                         color: Colors.white,
                       ),
